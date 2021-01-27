b32874b45c41580bd367fc0cf5d02328dd96203c
mvnuela <73135143+mvnuela@users.noreply.github.com>
2021-01-27 20:42:02 +0100

    ProductPanel & ClientPanel

--- shop-app/src/main/java/edu/pwr/db/view/CustomerAddPanel.java
+++ shop-app/src/main/java/edu/pwr/db/view/CustomerAddPanel.java
@@ -1,6 +1,87 @@
 package edu.pwr.db.view;
 
+import edu.pwr.db.model.Item;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class CustomerAddPanel extends JPanel {
+    protected final JButton add;
+    public CustomerAddPanel(){
+        JPanel panel = new JPanel();
+        GridBagConstraints gc = new GridBagConstraints();
+        panel.setLayout(new GridBagLayout());
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.insets = new Insets(20, 20, 20, 20);
+
+        JLabel jlName = new JLabel("Name: ");
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.gridwidth=1;
+        panel.add(jlName, gc);
+
+        JTextField inputName = new JTextField("Enter name              ");
+        gc.gridx = 1;
+        panel.add(inputName,gc);
+
+        JLabel jlSurname = new JLabel("Surname: ");
+        gc.gridx = 0;
+        gc.gridy = 1;
+        panel.add(jlSurname, gc);
+
+        JTextField inputSurname = new JTextField("Enter surname                ");
+        gc.gridx=1;
+        panel.add(inputSurname,gc);
+
+        JLabel jlAddress = new JLabel("Addres");
+        gc.gridx=0;
+        gc.gridy=2;
+        panel.add(jlAddress,gc);
+
+        JTextField inputAddress = new JTextField("Enter addres           ");
+        gc.gridx=1;
+        panel.add(inputAddress,gc);
+
+        add = new JButton("Add");
+        gc.gridy=3;
+        gc.gridwidth = 4;
+        panel.add(add, gc);
+        setLayout(new BorderLayout());
+        add(panel, BorderLayout.CENTER);
+
+        inputName.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                inputName.setText("");
+            }
+        });
+        inputSurname.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                inputSurname.setText("");
+
+            }
+        });
+        inputAddress.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                inputAddress.setText("");
+            }
+        });
+
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
 }
--- shop-app/src/main/java/edu/pwr/db/view/ProductAddPanel.java
+++ shop-app/src/main/java/edu/pwr/db/view/ProductAddPanel.java
@@ -1,8 +1,97 @@
 package edu.pwr.db.view;
 
+import edu.pwr.db.model.Item;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ProductAddPanel extends JPanel {
+    protected final JComboBox<Item> brands, colors, coverageLevels,types;
+    protected JTextField inputName;
+    protected final JButton add;
     public ProductAddPanel() {
+        JPanel panel = new JPanel();
+        GridBagConstraints gc = new GridBagConstraints();
+        panel.setLayout(new GridBagLayout());
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.insets = new Insets(20, 20, 20, 20);
+
+        JLabel jlBrand = new JLabel("brand: ");
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.gridwidth = 1;
+        panel.add(jlBrand, gc);
+
+        brands = new JComboBox<Item>();
+        brands.setPrototypeDisplayValue(Item.ANY);
+        gc.gridx = 1;
+        panel.add(brands, gc);
+
+        JLabel jlColor = new JLabel("color: ");
+        gc.gridx = 0;
+        gc.gridy = 1;
+        panel.add(jlColor, gc);
+
+        colors = new JComboBox<>();
+        gc.gridx = 1;
+        panel.add(colors, gc);
+
+        JLabel jlLevel = new JLabel("coverage level: ");
+        gc.gridx = 0;
+        gc.gridy = 2;
+        panel.add(jlLevel, gc);
+
+        coverageLevels = new JComboBox<>();
+        gc.gridx = 1;
+        panel.add(coverageLevels, gc);
+
+        JLabel jlTypes = new JLabel("type: ");
+        gc.gridx = 0;
+        gc.gridy = 3;
+        panel.add(jlTypes, gc);
+
+        types = new JComboBox<>();
+        gc.gridx = 1;
+        panel.add(types, gc);
+
+        JLabel jlName = new JLabel("name: ");
+        gc.gridx = 0;
+        gc.gridy = 4;
+        panel.add(jlName,gc);
+
+        JTextField inputName=new JTextField("Enter product name");
+        inputName.setFont(new Font("X", Font.ITALIC, 15));
+        gc.gridx=1;
+        panel.add(inputName,gc);
+
+        add = new JButton("Add");
+        gc.gridx = 0;
+        gc.gridy = 5;
+
+        gc.gridwidth = 2;
+        panel.add(add, gc);
+
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // pobieramy dane i dodajemy produkt
+            }
+        });
+
+        setLayout(new BorderLayout());
+        add(panel, BorderLayout.CENTER);
+        inputName.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                inputName.setText("");
+            }
+        });
+
+
     }
 }
