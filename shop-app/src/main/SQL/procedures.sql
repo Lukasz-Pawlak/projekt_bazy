226fa1b295f1c7bc37bfaed0d17433b3b04de8ec
Lukasz-Pawlak <lukasz.wieslaw.pawlak+1@gmail.com>
2021-01-27 19:28:34 +0100

    first 2 procedures reviewed & changed (syntax, logic & requirements fixes)

--- shop-app/src/main/SQL/procedures.sql
+++ shop-app/src/main/SQL/procedures.sql
@@ -1,37 +1,63 @@
 use shop;
 delimiter $$
 drop procedure if exists addProduct $$
-create procedure addProduct(in c int, in b int, in cL int, in t int, in n varchar(100))
+create procedure addProduct(in c varchar(30), in b varchar(30), in cLn varchar(30), in cLv int, in t varchar(20), in n varchar(100))
 begin
-	if c not in (select id from Colors) then
-    signal sqlstate '45000' set message_text="Wrong color";
+declare ci, bi, cLi, ti int;
+	if c not in (select name from Colors) then
+        insert into colors (name) values (c);
     end if;
-    if b not in (select id from Brands) then
-    signal sqlstate '45000' set message_text="Wrong brands";
+    if b not in (select name from Brands) then
+        insert into brands (name) values (b);
     end if;
-    if cL not in (select id from CoverageLevels) then
-    signal sqlstate '45000' set message_text="Wrong coverage Level";
+    if not exists (select * from CoverageLevels where name = cLn and numericValue = cLv) then
+        insert into coveragelevels (numericValue, name) values (cLv, cLn);
     end if;
-    if t not in (select id from Type) then
-    signal sqlstate '45000' set message_text="Wrong type";
+    if t not in (select name from types) then
+        insert into types (name) values (t);
     end if;
-    if n in(select name from Products) then
+    select id from types where name = t into ti;
+    select id from coveragelevels where name = cLn and numericValue = cLv into cLi;
+    select id from brands where name = b into bi;
+    select id from colors where name = c into ci;
+    if exists (
+        select * from products
+        where color = ci
+        and coverageLevel = cLi
+        and brand = bi
+        and type = ti
+    ) then
     signal sqlstate '45000' set message_text="This product already exists";
-    end if;
-	insert into Products (color,brand,coverageLevel,type)
-    values(c,b,cL,t);
-end $$
+    else
+	    insert into Products (color,brand,coverageLevel,type)
+        values(ci, bi, cLi, ti);
+	end if;
+end; $$
 delimiter ;
 
 delimiter $$
 drop procedure if exists addClient $$
 create procedure addClient(in n varchar(50), in s varchar(50), in a varchar(100))
 begin
- if (select name, surname, address from Clients where name=n and surname=s and address=a) is null then
-		insert into Clients(name,surname,addres)
-	 values (n,s,a);	
- else
+ if exists (select name, surname, address from Clients where name=n and surname=s and address=a) then
 	 signal sqlstate '45000' set message_text="Client already exists";
+ else
+	insert into Clients(name,surname,address)
+	values (n,s,a);
  end if;
-end $$
+end; $$
 delimiter ;
+
+-- saving for later since it's already here, definitely will get handy
+/*      select * from Products
+        inner join brands b2 on products.brand = b2.id
+        inner join colors c2 on products.color = c2.id
+        inner join coverageLevels c3 on products.coverageLevel = c3.id
+        inner join types t2 on products.type = t2.id
+        where
+            b2.name = b
+        and c2.name = c
+        and c3.name = cLn
+        and c3.numericValue = cLv
+        and t2.name = t
+ */
