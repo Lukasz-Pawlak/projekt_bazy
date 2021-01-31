5bd656bacf1282a1324edfb8d2d4c510dc54fb59
Lukasz-Pawlak <lukasz.wieslaw.pawlak+1@gmail.com>
2021-02-01 00:31:49 +0100

    admin panel functionality done

--- shop-app/src/main/java/edu/pwr/db/view/AdminPanel.java
+++ shop-app/src/main/java/edu/pwr/db/view/AdminPanel.java
@@ -3,30 +3,37 @@ package edu.pwr.db.view;
 import edu.pwr.db.model.Login;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
 public class AdminPanel extends JPanel {
     private final AppWindow appWindow;
-    private final JButton doBackup, restoreFromBackup;
+    private final JButton doBackup, restoreFromBackup, lunchCmd;
 
     public AdminPanel(AppWindow appWindow) {
         this.appWindow = appWindow;
         doBackup = new JButton("make backup");
         restoreFromBackup = new JButton("restore from backup");
+        lunchCmd = new JButton("lunch command line");
 
         setLayout(new GridBagLayout());
         var gc = new GridBagConstraints();
         gc.anchor = GridBagConstraints.CENTER;
         gc.weighty = gc.weightx = 1;
 
+        gc.gridx = gc.gridy = 0;
         add(doBackup, gc);
 
-        gc.gridx++;
+        gc.gridy++;
         add(restoreFromBackup, gc);
-        
+
+        gc.gridy++;
+        add(lunchCmd, gc);
+
         doBackup.addActionListener(e -> backup());
+        lunchCmd.addActionListener(e -> lunchCommandLine());
         restoreFromBackup.addActionListener(e -> restore());
     }
 
@@ -65,43 +72,65 @@ public class AdminPanel extends JPanel {
             if (processComplete == 0) {
                 JOptionPane.showMessageDialog(appWindow,
                         "backup done", "info", JOptionPane.INFORMATION_MESSAGE);
-            }
-            else {
+            } else {
                 JOptionPane.showMessageDialog(appWindow,
                         "backup failure", "info", JOptionPane.INFORMATION_MESSAGE);
             }
 
         } catch (IOException | InterruptedException ex) {
-            JOptionPane.showMessageDialog(null, "Error at Backuprestore" + ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error at backup: " + ex.getMessage());
         }
     }
 
     private void restore() {
+        try {
+            String dbName = "Shop";
+            String dbUser = Login.getUsername();
+            String dbPass = Login.getPassword();
 
+            String path;
+
+            JFileChooser chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new java.io.File("."));
+            chooser.setDialogTitle("select backup target directory");
+            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("sql backups", "sql");
+            chooser.setFileFilter(filter);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                path = chooser.getSelectedFile().toString();
+            } else {
+                JOptionPane.showMessageDialog(appWindow,
+                        "restore aborted", "info", JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+
+            String[] executeCmd = new String[]{"mysql", dbName, "-u" + dbUser, "-p" + dbPass, "-e", " source " + path};
+            Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+            int processComplete = runtimeProcess.waitFor();
+
+            if (processComplete == 0) {
+                JOptionPane.showMessageDialog(appWindow, "Successfully restored");
+            } else {
+                JOptionPane.showMessageDialog(appWindow, "Error at restoring");
+            }
+
+        } catch (IOException | InterruptedException | HeadlessException ex) {
+            JOptionPane.showMessageDialog(appWindow, "Error at restore: " + ex.getMessage());
+        }
     }
-/*
-    public static String getData(String host, String port, String user, String password, String db,String table) throws Exception {
 
-        //an "C:/xampp/mysql/bin/mysqldump" ---- location ito han mysqldump
+    private void lunchCommandLine() {
+        try {
+            String dbName = "Shop";
+            String dbUser = Login.getUsername();
+            String dbPass = Login.getPassword();
+            String[] executeCmd = new String[]{"cmd", "/c", "start", "cmd.exe", "/K", "mysql", dbName, "-u" + dbUser, "-p" + dbPass};
+            Runtime.getRuntime().exec(executeCmd);
 
-        Process run = Runtime.getRuntime().exec(
-                "C:/xampp/mysql/bin/mysqldump --host="  + host + " --port=" + port +
-                        " --user=" + user + " --password=" + password +
-                        " --compact --databases --add-drop-table --complete-insert --extended-insert " +
-                        "--skip-comments --skip-triggers "+ db+" --tables "+table);
+        } catch (IOException | HeadlessException ex) {
+            JOptionPane.showMessageDialog(appWindow, "Error at restore: " + ex.getMessage());
+        }
+    }
 
-        InputStream in = run.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        StringBuffer temp = new StringBuffer();
-        int count;
-        char[] cbuf = new char[BUFFER];
-
-        while ((count = br.read(cbuf, 0, BUFFER)) != -1)
-            temp.append(cbuf, 0, count);
-
-        br.close();
-        in.close();
-
-        return temp.toString();
-    }*/
 }
